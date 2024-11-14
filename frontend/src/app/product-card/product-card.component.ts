dda5df1836468e21010f5de0f262a3791d925001
Rakesh-608 <rakeshchenna2002@gmail.com>
2024-11-14 17:04:26 +0530

    cards updated

--- frontend/src/app/product-card/product-card.component.ts
+++ frontend/src/app/product-card/product-card.component.ts
@@ -3,11 +3,13 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../models/product';
+import {MatBadgeModule} from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CommonModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule, MatBadgeModule, MatIconModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
@@ -19,5 +21,20 @@ export class ProductCardComponent {
   // Emit event on click
   onSelect() {
     this.selectProduct.emit();
+  }
+
+
+
+  isInCart = false;
+  toggleCart() {
+    this.isInCart = !this.isInCart;
+    // You can add additional logic here to handle adding/removing from the cart
+    if (this.isInCart) {
+      // Logic for adding item to cart
+      console.log('Item added to cart');
+    } else {
+      // Logic for removing item from cart
+      console.log('Item removed from cart');
+    }
   }
 }
